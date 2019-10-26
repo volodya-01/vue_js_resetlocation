b23143c555cd08ce8a860531c24e2d2765bcae65
volodya <18356090933@163.com>
2019-10-26 11:43:36 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,62 @@
+<template>
+  <div id="app">
+      <router-view/>
+  <!--   <div class="navbox">
+      <div v-for="(item,index) in navroom" :key="index" class="itembox" @click="slet(index)">{{item}}</div>
+    </div>
+    <iframe :src="pathurl" frameborder="0" class="box"></iframe> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      navroom: [
+        "http://www.baidu.com",
+        "https://www.taobao.com",
+        "https://www.jd.com"
+      ],
+      pathurl: "http://www.baidu.com"
+    };
+  },
+  mounted() {
+    console.log(this.$router.options.routes.path);
+  },
+  methods:{
+    slet(index){
+      this.pathurl=this.navroom[index]
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+   width: 100vw;
+  height: 100vh;
+  overflow: hidden;
+  margin: 0;
+  padding: 0;
+}
+.navbox {
+  display: flex;
+  flex-flow: row;
+  justify-content: flex-start;
+  align-items: center;
+  width: calc(100vw - 1px);
+  height:58px;
+  border: 1px solid #ccc;
+  border-right: none;
+}
+.itembox {
+  width: calc(100vw / 3 - 3px);
+  height: 58px;
+  border-right: 1px solid #ccc;
+}
+.box {
+  width: 100vw;
+  height: calc(100vh - 60px);
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="hello">
+    <div class="outbox">
+      <div class="inbox" :style="isstyle">
+        <h1>{{ msg }}</h1>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      msg: "Welcome to Your Vue.js App",
+      isstyle: ''
+    };
+  },
+  mounted() {
+    var _this = this;
+    _this.getnumfun();
+    setInterval(() => {
+      _this.getnumfun();
+    }, 1000);
+  },
+  methods: {
+    getnumfun() {
+      var x=parseInt(Math.random()*100)
+      var y=parseInt(Math.random()*100)
+      this.resetlocation(x, y);
+    },
+    resetlocation(x, y) {
+      this.isstyle = `top:${y}px;left:${x}px`;
+      console.log("123")
+      console.log("456")
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.hello {
+  display: flex;
+  flex-flow: row;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+}
+.outbox {
+  width: 50vw;
+  height: 50vh;
+  border: 2px #444 solid;
+  position: relative;
+}
+.inbox {
+  display: flex;
+  flex-flow: row;
+  justify-content: center;
+  align-items: center;
+  width: 200px;
+  height: 200px;
+  background-color: red;
+  position: absolute;
+}
+h1 {
+  font-weight: normal;
+}
+</style>
